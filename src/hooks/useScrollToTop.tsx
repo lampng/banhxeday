31de04a48f52320676c465bae8a25396cd54dd9b
thangnguyen-ukpc <thang.nguyen@ukparkingcontrol.com>
2024-07-30 11:26:44 +0700

    Create a folder structure

---
+++ src/hooks/useScrollToTop.tsx
@@ -0,0 +1,11 @@
+import React, { useEffect } from 'react';
+
+export default function useScrollToTop() {
+    useEffect(() => {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth',
+        });
+    }, []);
+    return {};
+}
